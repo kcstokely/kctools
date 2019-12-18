3b336dcfc3252c09b9a0336f5fe791ad8555a1e7
kcstokely@gmail.com <kevin.stokely@mindgeek.com>
2019-12-18 08:33:47 -0500

    version # in setup

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r') as fp:
     
 setuptools.setup(
      name         = 'kctools',
-     version      = '0.0.7',
+     version      = '0.0.8',
      author       = 'kevin c. stokely',
      author_email = 'kcstokely@gmail.com',
      description  = 'miscellaneous elves',
